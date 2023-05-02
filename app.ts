5a4fe414b6f0e7640a21d39b671ce9f22de75fc0
AndrewLin95 <andrew.lin88@hotmail.com>
2023-05-02 18:33:44 -0400

    test api

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 import path from 'path';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 8080;
 const app: Application = express();
+app.use(bodyParser.json());
+
+app.get("/api/test", (req: Request, res: Response) => {
+  res.status(200).json({ message: "test"});
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
